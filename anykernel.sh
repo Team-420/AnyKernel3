790fa5149bf980438fdda47c6f30fcc451d43a9f
S133PY <thiviyan@gmail.com>
2020-06-15 22:33:40 +0530

    Whyred Nethunter

--- anykernel.sh
+++ anykernel.sh
@@ -1,61 +1,126 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
+# Customized for NetHunter
+
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=NetHunter kernel
 do.devicecheck=1
-do.modules=0
-do.systemless=1
-do.cleanup=1
+do.modules=1
+do.cleanup=0
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=whyred
+device.name2=redminote5
+device.name3=xiaomi note5
+device.name4=xiaomiredminote5
 device.name5=
+device.name6=
+device.name7=
+device.name8=
+device.name9=
+device.name10=
 supported.versions=
-supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
-
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
+## NetHunter additions
+
+SYSTEM="/system";
+SYSTEM_ROOT="/system_root";
+
+setperm() {
+	find "$3" -type d -exec chmod "$1" {} \;
+	find "$3" -type f -exec chmod "$2" {} \;
+}
+
+install() {
+	setperm "$2" "$3" "$home$1";
+	if [ "$4" ]; then
+		cp -r "$home$1" "$(dirname "$4")/";
+		return;
+	fi;
+	cp -r "$home$1" "$(dirname "$1")/";
+}
+
+[ -d $home/system/etc/firmware ] && {
+	install "/system/etc/firmware" 0755 0644 "$SYSTEM/etc/firmware";
+}
+
+[ -d $home/system/etc/init.d ] && {
+	install "/system/etc/init.d" 0755 0755 "$SYSTEM/etc/init.d";
+}
+
+[ -d $home/system/lib ] && {
+	install "/system/lib" 0755 0644 "$SYSTEM/lib";
+}
+
+[ -d $home/system/lib64 ] && {
+	install "/system/lib64" 0755 0644 "$SYSTEM/lib64";
+}
+
+[ -d $home/system/bin ] && {
+	install "/system/bin" 0755 0755 "$SYSTEM/bin";
+}
+
+[ -d $home/system/xbin ] && {
+	install "/system/xbin" 0755 0755 "$SYSTEM/xbin";
+}
+
+[ -d $home/data/local ] && {
+	install "/data/local" 0755 0644;
+}
+
+[ -d $home/ramdisk-patch ] && {
+	setperm "0755" "0750" "$home/ramdisk-patch";
+        chown root:shell $home/ramdisk-patch/*;
+	cp -rp $home/ramdisk-patch/* "$SYSTEM_ROOT/";
+}
+
+if [ ! "$(grep /init.nethunter.rc $SYSTEM_ROOT/init.rc)" ]; then
+  insert_after_last "$SYSTEM_ROOT/init.rc" "import .*\.rc" "import /init.nethunter.rc";
+fi;
+
+if [ ! "$(grep /dev/hidg* $SYSTEM_ROOT/ueventd.rc)" ]; then
+  insert_after_last "$SYSTEM_ROOT/ueventd.rc" "/dev/kgsl.*root.*root" "# HID driver\n/dev/hidg* 0666 root root";
+fi;
+
+ui_print "Applying additional anykernel installation patches";
+for p in $(find ak_patches/ -type f); do
+  ui_print "Applying $p";
+  . $p;
+done
+
+## End NetHunter additions
+
+## Trim partitions
+fstrim -v /cache;
+fstrim -v /data;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
 
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+if [ -d $ramdisk/.backup ]; then
+  patch_cmdline "skip_override" "skip_override";
+else
+  patch_cmdline "skip_override" "";
+fi;
 
 # end ramdisk changes
 
